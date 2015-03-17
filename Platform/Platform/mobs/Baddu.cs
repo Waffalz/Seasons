4a1ccc6d309ebb17bd40766345238e25546de1b0
michaelmilton99 <michael@themiltons.org>
2015-03-17 09:59:01 -0500

    Improved enemy behavior & new maps
    Baddu now changes direction when hitting a wall, added side walls to
    maps (needs collision debug to fix wall jump glitch), multiple levels
    added (all same right now, will be edited later)

--- Platform/Platform/mobs/Baddu.cs
+++ Platform/Platform/mobs/Baddu.cs
@@ -5,6 +5,8 @@ using System.Text;
 
 using Microsoft.Xna.Framework;
 
+using Platform.World;
+
 namespace Platform.Mobs
 {
     class Baddu:Mob, Behavior
@@ -29,9 +31,41 @@ namespace Platform.Mobs
         public void Behave(float timeDif){
             Vector2 dir = Mob.GetDirection(moveDirection);
             WalkVelocity += new Vector2(dir.X * WalkSpeed, 0);
+
+            int tilePositionX = (int)(position.X / Tile.TILE_WIDTH);
+            int tilePositionY = (int)(position.Y / Tile.TILE_WIDTH);
+
+            Tile nextTile = parent.Tiles[tilePositionY, tilePositionX + (int)(Mob.GetDirection(moveDirection).X)];
+
+
+            Entity nextPlace = new Entity();
+            nextPlace.Position = this.position + (velocity + WalkVelocity) * timeDif;
+            nextPlace.Size = this.size;
+
+            if (nextTile != null)
+            {
+                Entity lent = new Entity();
+                lent.Size = new Vector2(Tile.TILE_WIDTH, Tile.TILE_WIDTH);
+                lent.Position = new Vector2((tilePositionX + (int)(Mob.GetDirection(moveDirection).X)) * Tile.TILE_WIDTH + Tile.TILE_WIDTH / 2, (tilePositionY) * Tile.TILE_WIDTH + Tile.TILE_WIDTH / 2);
+                lent.Anchored = true;
+                lent.Gravity = false;
+                lent.Solid = true;
+
+                if (nextPlace.Collides(lent))
+                {
+                    if (moveDirection == MoveDirection.Left)
+                    {
+                        moveDirection = MoveDirection.Right;
+                    }
+                    else if (moveDirection == MoveDirection.Right)
+                    {
+                        moveDirection = MoveDirection.Left;
+                    }
+
+                }
+
+            }
             
-
-
 
         }
 
